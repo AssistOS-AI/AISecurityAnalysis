93eeb53d29599714b20b18f0ce21be92121d8084
iliseidaria <iliseidaria@yahoo.com>
2024-11-22 15:04:25 +0200

    friday version

--- web-components/aisecurityanalysis-page/aisecurityanalysis-page.js
+++ web-components/aisecurityanalysis-page/aisecurityanalysis-page.js
@@ -1,4 +1,4 @@
-const assistOSSDK = require("assistos")
+const assistOSSDK = require("assistos");
 const applicationModule = require('assistos').loadModule('application', {});
 
 export class AISecurityAnalysisPage {
@@ -6,41 +6,41 @@ export class AISecurityAnalysisPage {
         this.element = element;
         this.invalidate = invalidate;
         this.invalidate();
-        this.documentId = this.element.getAttribute("data-documentId")
-
+        this.documentId = this.element.getAttribute("data-documentId");
     }
 
     saveProject() {
-        // const llmmodule = assistOSSDK.loadModule("llm")
         alert("Project Saved Successfully!");
-
     }
 
     async beforeRender() {
-        
+        // Cod de initializare care se ruleaza inainte de redarea paginii (daca este necesar)
     }
 
     async afterRender() {
-
+        //  butonul de print este disponibil dupa redarea completa a paginii
+        const printButton = document.getElementById('print-button');
+        if (printButton) {
+            printButton.addEventListener('click', function () {
+                window.print(); // Deschide dialogul de imprimare
+            });
+        }
     }
 
-    async submitForm (_target){
-
+    async submitForm(_target) {
         try {
             await assistOS.loadifyFunction(async () => {
-                const bookGenerationData = {
-                    llm: "ChatGPT",
-                    size: 2,
-                    documentId: this.documentId
-                };
-
                 const formElement = this.element.querySelector("form");
                 const formData = await assistOS.UI.extractFormInformation(formElement);
+
+                // verif date formularului sunt valide
                 if (!formData.isValid) {
                     return assistOS.UI.showApplicationError("Invalid form data", "Please fill all the required fields", "error");
                 }
+
                 const planData = formData.data;
 
+                // Ruleaza fluxul de generare pentru proiect
                 const response = await applicationModule.runApplicationFlow(
                     assistOS.space.id,
                     "AISecurity",
@@ -48,6 +48,7 @@ export class AISecurityAnalysisPage {
                     planData
                 );
 
+                // Obtine documentId-ul si incarca pagina cu documentul generat
                 const documentId = response.data;
                 await assistOS.UI.changeToDynamicPage(
                     `space-application-page`,
